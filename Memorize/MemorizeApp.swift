d5c36dbeb6580519a7664e00980ee7ba2896e738
Anton Makeev <at-mak@mail.ru>
2020-11-29 14:18:17 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Admin on 29.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
